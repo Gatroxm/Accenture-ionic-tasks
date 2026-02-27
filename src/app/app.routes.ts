1b231315317140515d47ea0d983d6fa782cbc569
Gustavo Adolfo Muñoz Reyes <tavoxpau@gmail.com>
2026-02-27 18:02:24 -0500

    Refactor: Clean template and implement feature-based standalone structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./features/tasks/task-list/task-list.page')
+        .then(m => m.TaskListPage),
+  },
+  {
+    path: 'task-list',
+    loadComponent: () => import('./features/tasks/task-list/task-list.page').then( m => m.TaskListPage)
   },
 ];
